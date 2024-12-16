9539d1c834d8a901ad83f021c192b74fd12eaeed
Ali Amin <aly128@live.se>
2024-12-16 11:23:08 +0100

    Komplettering

--- Intro.js
+++ Intro.js
@@ -1,12 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Vänta 5 sekunder innan övergången startar
     setTimeout(() => {
-        // Lägg till 'fade-out'-klass för smidig övergång
         document.body.classList.add('fade-out');
-
-        // Vänta på övergången innan du laddar nästa sida
         setTimeout(() => {
-            window.location.href = "Startsida.html"; // Byt till din portfolios startsida
-        }, 1000); // Matchar övergångstiden i CSS
-    }, 2000); // Vänta 2 sekunder innan övergången startar
+            window.location.href = "Startsida.html";
+        }, 1000);
+    }, 2000);
 });
